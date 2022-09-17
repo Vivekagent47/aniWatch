7efe131d02afbf6735506658d80c7b097a77f6d2
Vivekagent47 <vivek.agent47@gmail.com>
2022-09-17 18:54:46 +0530

    Final setup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      blue: "#4048EF",
+      "dark-blue": "#040054",
+      gray: "#8C8C8C",
+    },
     extend: {},
   },
   plugins: [],
